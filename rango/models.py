5ad8f69af76cf1d715abd4889c8a97a74bc47788
Sean Davies <2243317D@student.gla.ac.uk>
2017-02-16 14:05:26 +0000

    working?

--- rango/models.py
+++ rango/models.py
@@ -4,17 +4,17 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name=models.CharField(max_length=128, unique=True)
-    likes=models.IntegerField(default=0)
-    views=models.IntegerField(default=0)
-    slug=models.SlugField(unique=True)
+    name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category,  self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = 'Category'
 
     def __str__(self):
         return self.name
@@ -30,14 +30,15 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
     def __unicode__(self):
         return self.title
 
 
 class UserProfile(models.Model):
-    user=models.OneToOneField(User)
-    website=models.URLField(blank=True)
-    picture=models.ImageField(upload_to='profile_images', blank=True)
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
 
     def __str__(self):
         return self.user.username
