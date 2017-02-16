3efff1fc20731f644e78d69019a99604fb91a27e
Sean Davies <2243317D@student.gla.ac.uk>
2017-02-16 14:53:01 +0000

    ch10 complete

--- rango/views.py
+++ rango/views.py
@@ -19,6 +19,7 @@ def get_server_side_cookie(request, cookie, default_val=None):
     return val
 
 def visitor_cookie_handler(request):
+
     visits = int(get_server_side_cookie(request, 'visits', '1'))
     last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
@@ -56,13 +57,17 @@ def index(request):
 
 def about(request):
 
+    context_dict={}
     if request.session.test_cookie_worked():
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
 
-    context_dict = {'boldmessage': "This tutorial has been put together by Sean Davies"}
+    request.session.set_test_cookie()
+    visitor_cookie_handler(request)
 
-    return render(request, 'rango/about.html', {})
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
 
